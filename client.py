7babadb4fec6b6c2c9698366f2e143286955c58d
JohnBensen1000 <jbensen@purdue.edu>
2023-01-21 10:50:09 -0500

    got sockets working two-way

--- client.py
+++ client.py
@@ -1,12 +1,19 @@
 import socket
-
-HOST = "172.20.10.7"  
-PORT = 4006
-
+import random
+HOST = "10.184.42.72"
+PORT = 4010
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     while True:
         data = s.recv(1024)
         print(data)
+        if random.randint(0, 5) == 3:
+            s.sendall(b"Hello from client")
+            print("sending data to server")
 
-print(f"Received {data!r}")
+
+
+
+
+
+
--- server.py
+++ server.py
@@ -1,41 +1,44 @@
 import socket
-from queue import Queue
 from threading import Thread
 import threading
 import time
-
-HOST = "172.20.10.7"
-PORT = 4006
-
+HOST = "10.184.42.72"
+PORT = 4010
 lock  = threading.Lock()
 conns = []
 
-def producer(out_q, socket):
+def listen_for_data(conn):
     while True:
-        conn, addr = s.accept()
+        data = conn.recv(1024)
+        print(data)
 
+def listen_for_connections(socket):
+    while True:
+        conn, addr = socket.accept()
         lock.acquire()
         conns.append(conn)
         lock.release()
-        
-def consumer(in_q, socket):
-    while True:
-        time.sleep(1)
-        for conn in conns:
-            try:
-                conn.send(b"hello")
-            except BrokenPipeError:
-                print("[ERROR] BrokenPipeError")
-          
+
+        t = Thread(target=listen_for_data, args=(conn, ))
+        t.start()
+
+def send_data_out(data):
+    for i, conn in enumerate(conns):
+        try:
+            conn.send(data)
+        except BrokenPipeError:
+            print("[ERROR] BrokenPipeError")
+            del conns[i]
+        except ConnectionAbortedError:
+            print("[ERROR] ConnectionAbortedError")
+            del conns[i]
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
-
-    q = Queue()
-    t1 = Thread(target=consumer, args=(q, s))
-    t2 = Thread(target=producer, args=(q, s))
+    t1 = Thread(target=listen_for_connections, args=(s, ))
     t1.start()
-    t2.start()
-
+    
     while True:
-        pass
+        time.sleep(1)
+        send_data_out(b"hello there")
